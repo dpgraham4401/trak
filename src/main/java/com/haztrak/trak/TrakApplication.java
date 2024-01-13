c519797c9d835275529ad21099368d96a0dcdc77
David Paul Graham <dpgraham4401@gmail.com>
2024-01-13 12:11:47 -0500

    initial commit

---
+++ src/main/java/com/haztrak/trak/TrakApplication.java
@@ -0,0 +1,13 @@
+package com.haztrak.trak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haztrak/trak/TrakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haztrak.trak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
