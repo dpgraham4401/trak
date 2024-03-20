b90bfb374a9fde467ea0ff9bbb816921ea8d8456
David Paul Graham <dpgraham4401@gmail.com>
2024-03-19 21:48:33 -0400

    add schema.sql, remove unneeded impl and interface

--- src/main/java/com/haztrak/trak/rcrainfosite/RcrainfoSite.java
+++ src/main/java/com/haztrak/trak/rcrainfosite/RcrainfoSite.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "rcrainfo_site")
+@Table(name = "rcrainfo_site", schema = "site")
 public class RcrainfoSite {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/haztrak/trak/rcrainfosite/repository/RcrainfoSiteQueryImpl.java
+++ src/main/java/com/haztrak/trak/rcrainfosite/repository/RcrainfoSiteQueryImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Repository
 class RcrainfoSiteQueryImpl implements RcrainfoSiteQuery {
 
-    private EntityManager entityManager;
+    private final EntityManager entityManager;
 
     @Autowired
     public RcrainfoSiteQueryImpl(EntityManager entityManager) {
